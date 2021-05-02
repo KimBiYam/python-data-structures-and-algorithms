441b0cf2242fd2146238852a0a61252ae0d7e58e
ChangHyunKim <hot9998@naver.com>
2021-05-02 13:49:38 +0900

    Add comment in is_anagram.py

--- collection/is_anagram.py
+++ collection/is_anagram.py
@@ -3,10 +3,12 @@ from collections import Counter
 
 def is_anagram(s1, s2):
     counter = Counter()
+    # counter 를 이용해서 각 문자의 갯수를 셈
     for c in s1:
         counter[c] += 1
     for c in s2:
         counter[c] -= 1
+    # 하나의 문자라도 일치하지 않는다면(0이 아니면) 두 문자열은 애너그램이 아니게 됨
     for i in counter.values():
         if i:
             return False
