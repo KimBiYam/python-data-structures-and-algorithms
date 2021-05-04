18a8de5bbb8b6de1b400127ef414649ca46427db
ChangHyunKim <hot9998@naver.com>
2021-05-04 20:18:55 +0900

    Add class compare with eq, hash method

---
+++ object-oriented-programming/hash_and_eq_OK.py
@@ -0,0 +1,26 @@
+class Symbol(object):
+    def __init__(self, value):
+        self.value = value
+
+    # __eq__ 메서드만 재정의하면 unhashable 에러가 나오게된다.
+    def __eq__(self, other):
+        if isinstance(self, other.__class__):
+            return self.value == other.value
+        else:
+            return NotImplemented
+
+    def __hash__(self):
+        return hash(self.value)
+
+
+if __name__ == "__main__":
+    x = Symbol("Py")
+    y = Symbol("Py")
+
+    symbols = set()
+    symbols.add(x)
+    symbols.add(y)
+
+    print(f'x is y : {x is y}')
+    print(f'x == y : {x == y}')
+    print(f'len(symbols) : {len(symbols)}')
